36b76dd4d2b39773b47ab29d3942312dd8c985a8
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 14:46:21 +0200

    Added Min-Max Sum

---
+++ HackerRank/Min-Max Sum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr = arr.OrderBy(x => x).ToList();
+
+        long minSum = 0;
+        long maxSum = 0;
+
+        for (int i = 1; i < arr.Count; i++)
+        {
+            maxSum += arr[i];
+        }
+
+        for (int i = arr.Count - 2; i >= 0; i--)
+        {
+            minSum += arr[i];
+        }
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
