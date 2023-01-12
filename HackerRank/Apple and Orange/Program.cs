ebf4e3ad6582d793190044494d3d56a4b803546c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 15:34:15 +0200

    Added Apple and Orange

---
+++ HackerRank/Apple and Orange/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+    {
+        int appleCount = 0;
+        int orangeCount = 0;
+
+        for (int i = 0; i < apples.Count; i++)
+        {
+            apples[i] += a;
+        }
+
+        for (int i = 0; i < oranges.Count; i++)
+        {
+            oranges[i] += b;
+        }
+
+        for (int i = 0; i < apples.Count; i++)
+        {
+            if (apples[i] >= s && apples[i] <= t)
+            {
+                appleCount++;
+            }
+        }
+
+        for (int i = 0; i < oranges.Count; i++)
+        {
+            if (oranges[i] >= s && oranges[i] <= t)
+            {
+                orangeCount++;
+            }
+        }
+
+        Console.WriteLine(appleCount);
+        Console.WriteLine(orangeCount);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int s = Convert.ToInt32(firstMultipleInput[0]);
+
+        int t = Convert.ToInt32(firstMultipleInput[1]);
+
+        string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int a = Convert.ToInt32(secondMultipleInput[0]);
+
+        int b = Convert.ToInt32(secondMultipleInput[1]);
+
+        string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int m = Convert.ToInt32(thirdMultipleInput[0]);
+
+        int n = Convert.ToInt32(thirdMultipleInput[1]);
+
+        List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
+
+        List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
