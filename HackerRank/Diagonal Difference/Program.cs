5723affb6cb41bb0cb383d51de68cc18a2981449
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 14:18:57 +0200

    Added Diagonal Difference

---
+++ HackerRank/Diagonal Difference/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < arr[0].Count; i++)
+        {
+            for (int j = 0; j < arr[0].Count; j++)
+            {
+                if (i == j)
+                {
+                    sum1 += arr[i][j];
+                }
+            }
+        }
+
+        int index1 = 0;
+        int index2 = arr[0].Count - 1;
+        
+        while (index2 >= 0)
+        {
+            sum2 += arr[index1][index2];
+            index1++;
+            index2--;
+        }
+
+        return Math.Abs(sum1 - sum2);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
