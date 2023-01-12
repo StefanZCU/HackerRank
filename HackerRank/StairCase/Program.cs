594b96d79b0d807ef4627320bcbcfa176fbf12dc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 14:34:30 +0200

    Added Staircase

---
+++ HackerRank/StairCase/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (j >= i)
+                {
+                    Console.Write('#');
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
