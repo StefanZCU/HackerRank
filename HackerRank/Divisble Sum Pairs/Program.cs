e9ac0314ae41ef14f7af1d6ed89a9ed241bc0fc9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-13 12:00:15 +0200

    Added Divisible Sum Pairs

---
+++ HackerRank/Divisble Sum Pairs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = 1;
+
+            for (int j = i; j < n; j++)
+            {
+                if (i + index < n)
+                {
+                    if ((ar[i] + ar[i + index]) % k == 0)
+                    {
+                        result++;
+                    }
+                }
+
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        textWriter.WriteLine(result);
+        
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
